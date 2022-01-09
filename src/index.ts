f45271b779e3f9e0b6a1e74a02585dbdba6f8093
NyCode <me@nycode.dev>
2022-01-09 15:20:38 +0100

    Add cors header

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { parseString } from 'xml2js';
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+}
+
 addEventListener('fetch', async (event) => {
   const url = new URL(event.request.url);
   const group = url.searchParams.get('group')?.replace('.', '/');
@@ -10,7 +16,8 @@ addEventListener('fetch', async (event) => {
       `Example: <a href="${exampleUrl}">${exampleUrl}</a>`, {
       status: 400,
       headers: {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/html',
+        ...corsHeaders
       }
     }));
     return;
@@ -21,16 +28,19 @@ addEventListener('fetch', async (event) => {
 async function handle(event: FetchEvent, group: string, module: string): Promise<Response> {
   const mavenMetadataUrl = `https://repo1.maven.org/maven2/${group}/${module}/maven-metadata.xml`;
   const response = await fetch(mavenMetadataUrl, {});
-  
+
   if (response.status != 200) {
     // Maven Artifact does not exist.
     return new Response(`${group}/${module} does not exist!`, {
-      status: 400
+      status: 400,
+      headers: corsHeaders
     });
   }
   const xml = await response.text();
   const metadata: any = await xml2js(xml);
-  return new Response(metadata.metadata.versioning[0].latest[0]);
+  return new Response(metadata.metadata.versioning[0].latest[0], {
+    headers: corsHeaders
+  });
 }
 
 function xml2js(xml: string): Promise<string> {
